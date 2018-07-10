8a22dcbad08940d6d0a09053c1b70550d67f1957
Sagrika <sagrikagemini@gmail.com>
2018-07-10 20:10:09 +0530

    PHP code

---
+++ bulletin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<link href="https://fonts.googleapis.com/css?family=Acme|Allura|Amatic+SC|Berkshire+Swash|Julius+Sans+One|Kaushan+Script|Merienda|Norican" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cinzel|Cookie|Dancing+Script|Lato|Patrick+Hand+SC|Rancho|Rock+Salt" rel="stylesheet"> 
+
+<style type="text/css">
+	.content{
+		font-size: 20px;
+		font-family: Rancho;
+	}
+	body {
+		background-color: GainsBoro;
+	}
+	.navbar-brand {
+		font-family: Julius Sans One;
+		font-size: 16px;
+		font-weight: bold;
+	}
+</style>
+
+<body>
+	<div class="container">
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">ANNOUNCEMENTS</a>
+        </div>
+    </nav>
+	</div.>
+	<br><br><br><br>
+	<div class="content">
+		<ul>
+			<li>Notice regarding Samaj Kalyan Scholarship. <a href="#">Click Here</a></li>
+			<li>Notice regarding Branch Change for 2nd Year. <a href="#">Click Here</a></li>
+			<li>Instructions for newly admitted students 2017-18. <a href="#">Click Here</a></li>
+			<li>Academic Calendar for Odd Semester. <a href="#">Click Here</a></li>
+			<li>Guest Faculty Recruitment for session 2017-18. <a href="#">Click Here</a></li>
+			<li>Schedule for special C.O. examination. <a href="#">Click Here</a></li>
+			<li>Anti Ragging Squad. <a href="#">Click Here</a></li>
+			<li>Notice regarding spot counseling. <a href="#">Click Here</a></li>
+			<li>Notice for special carry over exam. <a href="#">Click Here</a></li>
+			<li>Notice regarding class tests. <a href="#">Click Here</a></li><br>
+		</ul>
+	</div>
+
+</body>
